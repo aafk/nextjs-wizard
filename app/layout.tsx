aec60ac84e8fc3a68cc07243251eb37b5ce1fea3
comingwizard <talentdevstar@gmail.com>
2024-08-14 16:22:48 +0900

    remove mongo connect from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 import { Metadata } from "next";
-import connectMongo from "@/utils/connect-mongo";
 import Providers from "@/ui/ProgressBarProvider";
 import ThemeModeSelector from "@/ui/ModeSelector";
 import ThemeModeProvider from "@/ui/theme-mode-provider";
@@ -15,12 +14,11 @@ export const metadata: Metadata = {
   metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  await connectMongo();
   return (
     <html lang="en" className="dark">
       <body className={`${inter.className} antialiased`}>
